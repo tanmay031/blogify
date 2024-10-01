2ae77bf70feb7b5b28310495bd540f40e28f1506
Mahbubur Rahman Tanmay <mahbubur.tanmay@dsinnovators.com>
2024-10-01 21:43:08 +0200

    Registered the Post model in the admin interface with customization for list display and search functionality

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,11 @@
 # posts/admin.py
+
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','content','created_at', 'updated_at')  # Columns to display in the list view
+    search_fields = ('title', 'content')  # Search functionality
+
+# Register the Post model with the customized admin class
+admin.site.register(Post, PostAdmin)
